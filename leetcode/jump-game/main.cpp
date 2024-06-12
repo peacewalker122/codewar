6806fe4bcf685a166bfd99b06d0b07e7ed7c0d75
Daniel Anugerah <danielanugerah2004@gmail.com>
2024-06-13 01:18:51 +0800

    second attempt
    all testcases passed!

--- leetcode/jump-game/main.cpp
+++ leetcode/jump-game/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <ostream>
 #include <vector>
@@ -7,40 +8,22 @@ using namespace std;
 class Solution {
 public:
   // You are given an integer array nums. You are initially positioned at the
-  // array's first index, and each element in the array represents your maximum
-  // jump length at that position. Return true if you can reach the last index,
-  // or false otherwise. nums = [2,3,1,1,4]
+  // array's first index, and **each element in the array represents your
+  // maximum jump length at that position**. Return true if you can reach the
+  // last index, or false otherwise. nums = [2,3,1,1,4]
   bool canJump(vector<int> &nums) {
-    if (nums.size() == 1) {
-      return true;
-    }
-    // what's the subproblem
-    // some array were able to jump and some not, so the subproblem were findout
-    // those.
-    int cur_position = nums[0];
+    int n = nums.size();
+    int maxreach = 0; // this is to know how much we could achieve
 
-    if (cur_position == nums.size() - 1) {
-      return true;
-    }
-
-    vector<bool> valid_tojump(nums.size());
-
-    // Assign true to valid_tojump[i] if nums[i] > 0, otherwise false
-    for (size_t i = 0; i < nums.size(); ++i) {
-      valid_tojump[i] = nums[i] > 0;
-    }
-
-    for (int i = cur_position; cur_position < nums.size();) {
-      if (valid_tojump[i]) {
-        cur_position += nums[i];
-        std::cout << cur_position << std::endl;
-        i = cur_position;
-      } else {
+    for (int i = 0; i < n; i++) {
+      if (i > maxreach) {
         return false;
       }
+
+      max(maxreach, i + nums[i]);
     }
 
-    return true;
+    return maxreach >= nums.size() - 1;
   }
 };
 
